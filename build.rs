b60c6f9688656494ed67e8f591d09ec42e179f21
Gabriel Ford <gabe@fordltc.net>
2024-11-23 20:29:42 -0500

    Added feature for debug_gl_structs

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::{env, fs::File, path::PathBuf};
 
-use gl_generator::{Api, DebugStructGenerator, Fallbacks, Profile, Registry};
+use gl_generator::{Api, DebugStructGenerator, Fallbacks, Profile, Registry, StructGenerator};
 
 fn main() {
     let dest = PathBuf::from(&env::var("OUT_DIR").unwrap());
@@ -9,7 +9,13 @@ fn main() {
 
     let mut file = File::create(dest.join("gl_bindings.rs")).unwrap();
 
-    Registry::new(Api::Gl, (4, 6), Profile::Core, Fallbacks::None, [])
-        .write_bindings(DebugStructGenerator, &mut file)
-        .unwrap();
+    if cfg!(feature = "debug_gl_structs") {
+        Registry::new(Api::Gl, (4, 6), Profile::Core, Fallbacks::None, [])
+            .write_bindings(DebugStructGenerator, &mut file)
+            .unwrap();
+    } else {
+        Registry::new(Api::Gl, (4, 6), Profile::Core, Fallbacks::None, [])
+            .write_bindings(StructGenerator, &mut file)
+            .unwrap();
+    }
 }
